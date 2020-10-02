1c0da819fedd1ad2131740620a492718e983e70d
Talha Imran <talhaimran284@gmail.com>
2020-10-02 23:07:01 +0500

    Express starter basic setup

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+// Allow express to use static files
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send(`let's build a social website`));
+// Register a view engine
+app.set('view engine','ejs');
+
+app.get('/', (req, res) => {
+    res.render('home-page');
+});
 
 
 app.listen(3000, () => console.log('listening on port 3000!'));
