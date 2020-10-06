5cf6dac39fbd36ec75841cf19a7085c8190b5961
Talha Imran <talhaimran284@gmail.com>
2020-10-06 17:44:42 +0500

    Logging in and Logging out working quite fine by storing sesstions data to the db and destroying sessions data

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,12 +17,22 @@ exports.login = (req, res) =>{
         req.session.user = {
             username: user.data.username
         }
-        res.redirect('/');
+        // Allowing the session data to get saved to the database and then redirects to /
+        req.session.save(() => {
+            res.redirect('/');
+        });
     })
     .catch(err => res.send(err));
 }
 exports.logout = (req, res) =>{
+    // Destroy the current user session from the db
+    req.session.destroy(function() {
+
+        // Once the session data gets destroyed show the home page
+        res.redirect('/');
+    });
 }
+
 
 
 exports.homePage = (req, res) =>{
--- routes/routes.js
+++ routes/routes.js
@@ -8,13 +8,16 @@ const router = express.Router();
 // App Controllers
 const userController = require('../controllers/userController');
 
-// Listen for /social-web/ end point
+// Listen for / end point
 router.get('/', userController.homePage);
 
-// Listen for /social-web/create-user
+// Listen for /create-user
 router.post('/create-user', userController.register);
 
-// Listen for /social-web/create-user
+// Listen for /login
 router.post('/login', userController.login);
+
+// Listen for /logout
+router.post('/logout', userController.logout);
 
 module.exports = router;
